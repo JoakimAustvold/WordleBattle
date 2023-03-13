5e589b42a7199072bade6f032db024655f4b9ccd
millieba <millieba@hotmail.com>
2023-03-13 14:40:32 +0100

    #4 Added some variables to PlayState and SettingsState

--- wordleBattleProject/core/src/com/mygdx/game/states/PlayState.java
+++ wordleBattleProject/core/src/com/mygdx/game/states/PlayState.java
@@ -2,8 +2,18 @@ package com.mygdx.game.states;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
 public class PlayState extends State { // Called WordleGame in first draft of diagrams
     private static PlayState instance = new PlayState(GameStateManager.getInstance());
+    private boolean isDuel;
+    private boolean isGameOver;
+    private int turn;
+    private String inviteCode;
+    private String[] guessesPlayer1;
+    private String[] guessesPlayer2;
+    //private Player player1;
+    //private Player player2;
+
 
     private PlayState(GameStateManager gsm) {
         super(gsm);
--- wordleBattleProject/core/src/com/mygdx/game/states/SettingsState.java
+++ wordleBattleProject/core/src/com/mygdx/game/states/SettingsState.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SettingsState extends State {
     private static SettingsState instance = new SettingsState(GameStateManager.getInstance());
+    private int volume;
+    private boolean isDarkMode;
 
     private SettingsState(GameStateManager gsm) {
         super(gsm);
