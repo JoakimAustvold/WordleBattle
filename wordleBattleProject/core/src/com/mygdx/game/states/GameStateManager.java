ad1d500f12cf13eac4a94927f9376631c2eabbb1
Karen Villmones <karenvi@stud.ntnu.no>
2023-03-13 13:53:26 +0100

    Branch for #4, added States package.

---
+++ wordleBattleProject/core/src/com/mygdx/game/states/GameStateManager.java
@@ -0,0 +1,33 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import java.util.Stack;
+
+public class GameStateManager {
+    private Stack<State> states;
+
+    public GameStateManager(){
+        states = new Stack<State>();
+    }
+
+    public void push(State state){
+        states.push(state);
+    }
+
+    public void pop(){
+        states.pop();
+    }
+
+    public void set(State state){
+        states.pop();
+        states.push(state);
+    }
+
+    public void update(float dt){ // Argument: delta time
+        states.peek().update(dt);
+    }
+
+    public void render(SpriteBatch sb){ // Argument: the sprite batch, everything we want to render
+        states.peek().render(sb);
+    }
+}
