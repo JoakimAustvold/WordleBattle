1eb211a8d06d0f05bd4ae9b1d55f1658fb3707c3
thomawj <thomawj@ntnu.no>
2023-03-20 13:22:53 +0100

    5- Created wordgrid class

---
+++ wordleBattleProject/core/src/com/mygdx/game/Wordgrid.java
@@ -0,0 +1,15 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public class Wordgrid {
+    private Table gridTable;
+
+    public Wordgrid() {
+        this.gridTable = new Table();
+        this.gridTable.setSize(5, 6);
+        //TODO: Style table
+    }
+
+
+}
