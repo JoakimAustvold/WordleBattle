ec575b784ea56ef2537fc16e7b8ac3532aa51183
Marcus Benjamin Ravnå Birkeland <36818485+marcusbirkeland@users.noreply.github.com>
2023-03-21 01:45:24 +0100

    Remove unused import

--- wordleBattleProject/core/src/com/mygdx/game/exception/StateException.java
+++ wordleBattleProject/core/src/com/mygdx/game/exception/StateException.java
@@ -1,6 +1,5 @@
 package com.mygdx.game.exception;
 
-import java.security.InvalidParameterException;
 
 public class StateException extends IllegalArgumentException {
     public StateException(String errorMessage){
