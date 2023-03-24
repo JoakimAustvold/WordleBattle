29a0837f0b9c81a735ecd6a37971bc54e3d59993
Karen Villmones <karenvi@stud.ntnu.no>
2023-03-24 16:29:04 +0100

    #1 - tried to make the text more responsive

--- wordleBattleProject/core/src/com/mygdx/game/view/SingleplayerGameView.java
+++ wordleBattleProject/core/src/com/mygdx/game/view/SingleplayerGameView.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.view;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.exception.StateException;
@@ -18,7 +19,7 @@ public class SingleplayerGameView extends View{
     @Override
     public void render(State state, SpriteBatch spriteBatch) {
         if (state instanceof PlayState) {
-            font.draw(spriteBatch, ((PlayState) state).getWord(), 450, 1000);
+            font.draw(spriteBatch, ((PlayState) state).getWord(), Gdx.graphics.getWidth()/(((PlayState) state).getWord().length()-2), Gdx.graphics.getHeight() - 45);
         } else {
             throw new StateException("Wrong state type! Please provide a PlayState.");
         }
