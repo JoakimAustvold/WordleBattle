83c99f6661acf0a47755ef1a14be980ee66ea6a7
millieba <millieba@hotmail.com>
2023-03-24 17:20:04 +0100

    #1 Improved formatting of keyboard

--- wordleBattleProject/core/src/com/mygdx/game/view/SingleplayerGameView.java
+++ wordleBattleProject/core/src/com/mygdx/game/view/SingleplayerGameView.java
@@ -15,23 +15,24 @@ import com.mygdx.game.model.states.State;
 
 public class SingleplayerGameView extends View {
 
-    private final float buttonWidth = Gdx.graphics.getWidth() / 10f;
-    private final float buttonHeight = Gdx.graphics.getHeight() / 12f;
+    private final float buttonWidth = Gdx.graphics.getWidth() / 16f;
+    private final float buttonHeight = Gdx.graphics.getHeight() / 20f;
     private final float buttonPadding = buttonWidth / 5f;
 
-    private final String[] buttonValues = {"q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-            "a", "s", "d", "f", "g", "h", "j", "k", "l",
-            "z", "x", "c", "v", "b", "n", "m", "<--", "Enter"};
+    private final String[][] buttonValues = {
+            {"q", "w", "e", "r", "t", "y", "u", "i", "o", "p"},
+            {"a", "s", "d", "f", "g", "h", "j", "k", "l"},
+            {"Enter", "z", "x", "c", "v", "b", "n", "m", "<--"}
+    };
 
-    private final TextButton[] buttons = new TextButton[buttonValues.length];
-
+    private final TextButton[][] buttons = new TextButton[buttonValues.length][];
     private final SpriteBatch batch = new SpriteBatch();
     private final BitmapFont font = new BitmapFont();
     private final KeyboardInput keyboardInput;
     private final Stage stage = new Stage();
 
     public SingleplayerGameView() {
-        font.getData().setScale(6,6);
+        font.getData().setScale(6, 6);
         font.setColor(Color.BLACK);
 
         keyboardInput = new KeyboardInput();
@@ -43,16 +44,20 @@ public class SingleplayerGameView extends View {
         stage.addActor(table);
 
         for (int i = 0; i < buttonValues.length; i++) {
-            String buttonValue = buttonValues[i];
-            TextButton button = new TextButton(buttonValue, getButtonStyle());
-            button.getLabel().setFontScale(buttonWidth / 20f);
-            buttons[i] = button;
+            TextButton[] rowButtons = new TextButton[buttonValues[i].length];
+            for (int j = 0; j < buttonValues[i].length; j++) {
+                String buttonValue = buttonValues[i][j];
+                TextButton button = new TextButton(buttonValue, getButtonStyle());
+                button.getLabel().setFontScale(buttonWidth / 20f);
+                rowButtons[j] = button;
+            }
+            buttons[i] = rowButtons;
         }
 
-        for (int i = 0; i < buttonValues.length-1; i += 3) {
-            table.add(buttons[i]).size(buttonWidth, buttonHeight).pad(buttonPadding);
-            table.add(buttons[i+1]).size(buttonWidth, buttonHeight).pad(buttonPadding);
-            table.add(buttons[i+2]).size(buttonWidth, buttonHeight).pad(buttonPadding);
+        for (TextButton[] rowButtons : buttons) {
+            for (TextButton button : rowButtons) {
+                table.add(button).size(buttonWidth, buttonHeight).pad(buttonPadding);
+            }
             table.row();
         }
     }
@@ -63,7 +68,7 @@ public class SingleplayerGameView extends View {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         if (state instanceof PlayState) {
-            font.draw(spriteBatch, ((PlayState) state).getWord(), Gdx.graphics.getWidth()/(((PlayState) state).getWord().length()-2), Gdx.graphics.getHeight() - 45);
+            font.draw(spriteBatch, ((PlayState) state).getWord(), Gdx.graphics.getWidth() / (((PlayState) state).getWord().length() - 2), Gdx.graphics.getHeight() - 45);
         } else {
             throw new StateException("Wrong state type! Please provide a PlayState.");
         }
