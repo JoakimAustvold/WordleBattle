ba215ad3d39c1470a8aa2c2746fe5dcce2565077
Thomas <thomawj@stud.ntnu.no>
2023-04-09 21:46:53 +0200

    Added skin composer files

--- wordleBattleProject/core/src/com/mygdx/game/Wordgrid.java
+++ wordleBattleProject/core/src/com/mygdx/game/Wordgrid.java
@@ -1,5 +1,7 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -9,7 +11,9 @@ public class Wordgrid {
     private Skin skin;
 
     public Wordgrid() {
+        //TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("style.atlas"));
         skin = new Skin();
+        //skin.addRegions(atlas);
         this.gridTable = new Table();
         this.gridTable.setSize(5, 6);
         //TODO: Style table
@@ -18,7 +22,7 @@ public class Wordgrid {
 
     public void TestTable() {
         Label label = new Label("E", skin);
-        this.gridTable.add(label).pad(10).center();
+        this.gridTable.add(label).width(100);
     }
 
 }
