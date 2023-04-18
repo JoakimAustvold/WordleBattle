c0efe601dfa110443a4371705279245ca0e0d199
mbbirkel <mbbirkel@stud.ntnu.no>
2023-04-18 14:25:48 +0200

    add letter to alphabet string(:

--- wordleBattleProject/core/src/com/mygdx/game/view/letters/LetterMap.java
+++ wordleBattleProject/core/src/com/mygdx/game/view/letters/LetterMap.java
@@ -5,13 +5,19 @@ import com.badlogic.gdx.graphics.Texture;
 
 import java.util.HashMap;
 
+/**
+ * Contains a map for all letter-textures.
+ * Use for getting textures to draw in a wordle-game.
+ * @author Marcus Birkeland
+ * @version 18.04.2023
+ */
 public class LetterMap {
 
     private HashMap<String, Texture> textureMap;
 
     public LetterMap(){
         textureMap = new HashMap<>();
-        String charsString = "abcdefghijklmnopqrstuvxyz";
+        String charsString = "abcdefghijklmnopqrstuvwxyz";
         for(int i = 0; i < charsString.length(); i++){
             char c = charsString.charAt(i);
             textureMap.put(c+"", new Texture(Gdx.files.internal("textures/letters/"+c+".png")));
