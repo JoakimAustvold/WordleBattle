02a2eeb7f1e8ac605275129c409be2388a56adfa
Thomas <thomawj@stud.ntnu.no>
2023-04-19 17:44:04 +0200

    #21 - Added word drawing loop

--- wordleBattleProject/core/src/com/mygdx/game/view/multiplayer/MultiplayerWordInputView.java
+++ wordleBattleProject/core/src/com/mygdx/game/view/multiplayer/MultiplayerWordInputView.java
@@ -2,8 +2,8 @@ package com.mygdx.game.view.multiplayer;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.mygdx.game.model.states.SingleplayerGameState;
 import com.mygdx.game.model.states.State;
+import com.mygdx.game.model.states.multiplayer.MultiplayerWordInputState;
 import com.mygdx.game.view.SingleplayerGameView;
 
 public class MultiplayerWordInputView extends SingleplayerGameView {
@@ -15,7 +15,12 @@ public class MultiplayerWordInputView extends SingleplayerGameView {
     @Override
     public void render(State state, SpriteBatch spriteBatch) {
         //TODO: render word
-        stage.act(Gdx.graphics.getDeltaTime());
+        MultiplayerWordInputState currentState = (MultiplayerWordInputState) state;
+        String currentText = currentState.getKeyboardInput().getCurrentText();
+        for (int i = 0; i < currentText.length(); i++) {
+            spriteBatch.draw(letterMap.getTexture(currentText.charAt(i)+""),
+                    (Gdx.graphics.getWidth() / (WORD_POS_X_DIVISOR + 1.5f)) + (i * 150), (Gdx.graphics.getHeight() - 110.0f - WORD_DELTA_Y) - WORD_DELTA_Y);
+        }
         stage.draw();
     }
 
