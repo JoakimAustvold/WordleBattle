dd8b1819b0fe328594321328704668d34c0f93b6
Thomas <thomawj@stud.ntnu.no>
2023-04-19 20:03:53 +0200

    #21 - Added event handler for submit button

--- wordleBattleProject/core/src/com/mygdx/game/view/multiplayer/MultiplayerWordInputView.java
+++ wordleBattleProject/core/src/com/mygdx/game/view/multiplayer/MultiplayerWordInputView.java
@@ -2,10 +2,15 @@ package com.mygdx.game.view.multiplayer;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.mygdx.game.controller.ControllerManager;
+import com.mygdx.game.controller.multiplayer.JoinLobbyController;
 import com.mygdx.game.model.states.State;
 import com.mygdx.game.view.View;
 
@@ -30,11 +35,22 @@ public class MultiplayerWordInputView extends View {
     public void setup() {
         Gdx.input.setInputProcessor(stage);
         final TextField wordField = new TextField("", skin);
+        TextButton submitButton = new TextButton("Submit", skin);
         stage.addActor(wordField);
+        stage.addActor(submitButton);
         wordField.setPosition((float) (Gdx.graphics.getWidth()*0.5-Gdx.graphics.getWidth()*0.2), (float) (Gdx.graphics.getHeight() * 0.6));
         wordField.setSize((float) (Gdx.graphics.getWidth()*0.4), (float) (Gdx.graphics.getHeight() * 0.05));
         wordField.setMessageText("word:");
         wordField.setMaxLength(5);
+        submitButton.setPosition((float) (Gdx.graphics.getWidth() *0.5 -Gdx.graphics.getWidth()*0.2), (float) (Gdx.graphics.getHeight()*0.1));
+        submitButton.setSize((float) (Gdx.graphics.getWidth()*0.4), (float) (Gdx.graphics.getHeight() * 0.05));
+
+        submitButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                //TODO: Submit word to firebase and open the Multiplayer Game View
+            }
+        });
     }
 
     @Override
