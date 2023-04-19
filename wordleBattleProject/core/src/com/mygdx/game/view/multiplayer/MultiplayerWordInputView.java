3eb9ed3ba78f583f8aa420fead3a5113d950afe5
mbbirkel <mbbirkel@stud.ntnu.no>
2023-04-19 23:44:57 +0200

    #21 refactor event-listener to controller. Use wordValidator instead of inline method

--- wordleBattleProject/core/src/com/mygdx/game/view/multiplayer/MultiplayerWordInputView.java
+++ wordleBattleProject/core/src/com/mygdx/game/view/multiplayer/MultiplayerWordInputView.java
@@ -25,12 +25,17 @@ public class MultiplayerWordInputView extends View {
     private BitmapFont font;
     private String errormessage;
 
+    public TextButton submitButton;
+    public TextField wordField;
+
+
     public MultiplayerWordInputView() {
         this.stage = new Stage(new ScreenViewport());
         this.skin = new Skin(Gdx.files.internal("default/skin/uiskin.json"));
         this.skin.getFont("default-font").getData().setScale(4f,4f);
         font = new BitmapFont();
         errormessage = "Enter your word";
+        submitButton = new TextButton("Submit", skin);
         setup();
     }
     @Override
@@ -38,14 +43,13 @@ public class MultiplayerWordInputView extends View {
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1/30f));
         stage.draw();
         font.getData().setScale(6);
-        font.draw(spriteBatch, errormessage,(float) (Gdx.graphics.getWidth()*0.5-Gdx.graphics.getWidth()*0.2), (float) (Gdx.graphics.getHeight() * 0.5));
+        font.draw(spriteBatch, errormessage,(float) (Gdx.graphics.getWidth()*0.5-Gdx.graphics.getWidth()*0.3), (float) (Gdx.graphics.getHeight() * 0.5));
     }
 
     @Override
     public void setup() {
         Gdx.input.setInputProcessor(stage);
-        final TextField wordField = new TextField("", skin);
-        TextButton submitButton = new TextButton("Submit", skin);
+        wordField = new TextField("", skin);
         stage.addActor(wordField);
         stage.addActor(submitButton);
         wordField.setPosition((float) (Gdx.graphics.getWidth()*0.5-Gdx.graphics.getWidth()*0.2), (float) (Gdx.graphics.getHeight() * 0.6));
@@ -54,27 +58,15 @@ public class MultiplayerWordInputView extends View {
         wordField.setMaxLength(5);
         submitButton.setPosition((float) (Gdx.graphics.getWidth() *0.5 -Gdx.graphics.getWidth()*0.2), (float) (Gdx.graphics.getHeight()*0.1));
         submitButton.setSize((float) (Gdx.graphics.getWidth()*0.4), (float) (Gdx.graphics.getHeight() * 0.05));
-
-        submitButton.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                String submittedword = wordField.getText();
-                Words words = json.fromJson(Words.class, Gdx.files.internal("words/wordle_answers_eng.json"));
-                String[] wordList = words.getWords();
-                if (submittedword.length() != 5) {
-                    errormessage = "Error: word is too short";
-                }
-                else if (!Arrays.asList(wordList).contains(submittedword)) {
-                    errormessage = "Error: word is not valid";
-                }
-                //TODO: Submit word to firebase and open the Multiplayer Game View
-            }
-        });
     }
 
     @Override
     public void dispose() {
         stage.dispose();
         skin.dispose();
+    }
+
+    public void setErrormessage(String errormessage) {
+        this.errormessage = errormessage;
     }
 }
